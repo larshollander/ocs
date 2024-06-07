ae8f8a2fe5e52a9ae4c7040d6b2ebab68239189c
Djagur <64599344+Djagur@users.noreply.github.com>
2024-06-07 11:19:44 +0200

    Update hosts.py

--- hosts.py
+++ hosts.py
@@ -75,7 +75,7 @@ def get_hosts(interface, range_, timeout):
 
     # send the packet and store replies
     results = srp(packet, timeout=timeout, iface=interface, verbose=0)[0]
-    replies = [result[ARP] for result in results[0]]
+    replies = [result[1][ARP] for result in results]
 
     for reply in replies:
 
