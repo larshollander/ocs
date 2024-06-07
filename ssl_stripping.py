2415acc146b6be06f4484cf621d68da4a0134728
cjgvberg <c.j.g.v.berg@student.tue.nl>
2024-06-07 10:36:24 +0200

    Update ssl_stripping.py

--- ssl_stripping.py
+++ ssl_stripping.py
@@ -4,12 +4,14 @@ import multiprocessing
 import cryptography    #required for tls
 
 load_layer("tls")    #enables tls for the https connection with the server
+load_layer("http")    #also useful
 
-class SslRemover(multiprocessing.Process):
+class SslRemover(multiprocessing.Process, url_to_strip):
 
     def __init__(self, ip_victim, queue_num):
         
         multiprocessing.Process.__init__(self)
+        self.url_to_strip = url_to_strip
 
         self.queue         = NetfilterQueue()
         self.iprule_add    = "iptables -I FORWARD -p tcp -d {} -j NFQUEUE --queue_num {}".format(ip_victim, queue_num)
@@ -19,4 +21,16 @@ class SslRemover(multiprocessing.Process):
         self.queue.bind(queue_num, self.handle_packet)
 
     def handle_packet(self, packet):
+        packet_scapy = IP(packet_nfqueue.get_payload())
+
+        
         packet.accept()
+
+    def stripped_victim_automation(self):
+        # socket = TCP_client.tcplink(HTTP, <>, 80)
+
+
+    def tls_client_automation(self):
+        greeting = TLSClientHello(ciphers=<int code of the cipher suite>)
+        connection = TLSClientAutomaton(dport=50000, client_hello=ch)
+        connection.run()
